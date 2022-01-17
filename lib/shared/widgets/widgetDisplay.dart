20309805a242c07d27f400bd6fdab51e50439717
commonutility <eric.w.zhu@gmail.com>
2022-01-16 21:20:33 -0500

    1/16 postCard merge

---
+++ lib/shared/widgets/widgetDisplay.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class imageContainer extends StatelessWidget {
+
+  String imageString;
+
+  imageContainer ({Key? key, required this.imageString}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    margin: const EdgeInsets.only(left: 5.0, right: 5.0, top: 10.0, bottom: 10.0),
+    child:Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.network(imageString),
+        ],
+      )
+
+    );
+  }
+}
