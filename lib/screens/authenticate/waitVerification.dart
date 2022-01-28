2ee19423fcc249228207e9478ff55c3cf83365e6
commonutility <eric.w.zhu@gmail.com>
2022-01-27 20:26:08 -0500

    1/27 LinearGradient

--- lib/screens/authenticate/waitVerification.dart
+++ lib/screens/authenticate/waitVerification.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:hs_connect/screens/authenticate/registerUser.dart';
+import 'package:hs_connect/shared/constants.dart';
 import 'dart:async';
 
 import 'package:hs_connect/shared/pageRoutes.dart';
 import 'package:hs_connect/shared/pixels.dart';
 import 'package:provider/provider.dart';
+
+import 'authBar.dart';
 
 class WaitVerification extends StatefulWidget {
   const WaitVerification({Key? key, required this.domain}) : super(key: key);
@@ -44,28 +47,81 @@ class _WaitVerificationState extends State<WaitVerification> {
     final colorScheme = Theme.of(context).colorScheme;
 
     return Scaffold(
-      body: Center(
-        child: Column(
+        body: Stack(
           children: [
-            Container(
-              height: 625*hp,
-              width: 314*hp,
-              decoration: new BoxDecoration(
-                color: colorScheme.secondary,
-                shape: BoxShape.circle,
-              ),
+          Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+          Container(
+          height: 110*hp,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            gradient: Gradients.blueRed(begin: Alignment.topLeft , end: Alignment.bottomRight),
+          ),
+          child: SizedBox(),
+        ),
+        SizedBox(height: 10*hp),
+        Row(
+          children: [
+            SizedBox(width: 10*hp),
+            TextButton(
+                onPressed: (){
+                },
+                child:Text(
+                  "Cancel",
+                  style: ThemeText.inter(fontWeight: FontWeight.normal,
+                      fontSize: 16*hp, color: Colors.grey),
+                )
             ),
-            SizedBox(height: 703*hp),
-            Text("Make your Account", style: Theme.of(context).textTheme.headline5),
-            SizedBox(height:15*hp),
-            Text('Register with your school email.',
-                style: Theme.of(context).textTheme.subtitle1?.copyWith(color: colorScheme.secondary)),
-            SizedBox(height: 39*hp),
           ],
         ),
-      ),
-    );
-  }
+        SizedBox(height: 30*hp),
+        Center(
+            child:
+            Column(
+              children: [
+                SizedBox(
+                  height: 80 *hp,
+                  child:
+                  Image.asset('assets/logo1background.png'),
+                ),
+                SizedBox(height: 20*hp),
+                Text(
+                  'We sent you a link',
+                  style: ThemeText.inter(fontWeight: FontWeight.w700, fontSize: 28*hp, color: Colors.black //TODO: Convertto HP
+                  ),
+                ),
+                SizedBox(height:20*hp),
+                Center(
+                  child:Text("Check your school email to verify your account.",
+                      style: Theme.of(context).textTheme.subtitle1?.copyWith(color: colorScheme.onSurface, fontSize: 14)),
+                ),
+                SizedBox(height:5*hp),
+                Center(
+                  child:Text("This info is never shown.",
+                      style: Theme.of(context).textTheme.subtitle1?.copyWith(color: colorScheme.onSurface, fontSize: 14)),
+                ),
+                /*GradientText(
+                        'Sign up',
+                        style: ThemeText.inter(fontWeight: FontWeight.w700, fontSize: 28*hp, //TODO: Convertto HP
+                        ),
+                        gradient: Gradients.blueRed(),
+                      ),  */                          SizedBox(height:15*hp),
+                SizedBox(height: 50*hp),
+              ],
+            )
+          ),
+      ]),
+            Positioned(
+              bottom:0,
+              left:0,
+              child: AuthBar(buttonText: "Register",
+                  onPressed: () async {
+                  }),
+            ),
+          ])
+  );
+}
 
   Future<void> checkEmailVerified() async {
     user = auth.currentUser;
