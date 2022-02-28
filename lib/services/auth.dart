1a72b25a0c2a7462bac9073a824672b4c687b0bf
commonutility <eric.w.zhu@gmail.com>
2022-02-28 15:50:15 -0500

    Auth Cleanup

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -29,6 +29,27 @@ class AuthService {
     }
   }
 
+  Future<bool> checkIfEmailInUse(String emailAddress) async {
+    try {
+      // Fetch sign-in methods for the email address
+      final list = await FirebaseAuth.instance.fetchSignInMethodsForEmail(emailAddress);
+
+      // In case list is not empty
+      if (list.isNotEmpty) {
+        // Return true because there is an existing
+        // user using the email address
+        return true;
+      } else {
+        // Return false because email adress is not in use
+        return false;
+      }
+    } catch (error) {
+      // Handle error
+      // ...
+      return true;
+    }
+  }
+
   // sign in with username & password
   Future<dynamic> signIn(String email, String password) async {
     try {
