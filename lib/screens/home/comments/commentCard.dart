1b700549b7ad776e70a6395583aa6b53e1f4ea5d
commonutility <eric.w.zhu@gmail.com>
2022-03-01 15:47:35 -0500

    Auth Cleanup

--- lib/screens/home/comments/commentCard.dart
+++ lib/screens/home/comments/commentCard.dart
@@ -41,6 +41,9 @@ class _CommentCardState extends State<CommentCard> {
   Color? creatorGroupColor;
   String? creatorDomainName;
   int? creatorScore;
+  bool replySubject = false;
+
+
 
   @override
   void initState() {
@@ -85,137 +88,143 @@ class _CommentCardState extends State<CommentCard> {
 
     final localCreatorName = creatorName ?? '';
     final localCreatorGroupName = creatorDomainName ?? '';
-    return Container(
-        padding: EdgeInsets.symmetric(horizontal: 15),
-        child: Column(mainAxisSize: MainAxisSize.min, crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-          Row(
-            children: [
-              Container(
-                alignment: Alignment.centerLeft,
-                height: 33,
-                child: TextButton(
-                    style: TextButton.styleFrom(
-                        splashFactory: NoSplash.splashFactory,
-                        padding: EdgeInsets.zero,
-                        alignment: Alignment.centerLeft),
-                    onPressed: () {
-                      if (creatorName != null) {
-                        showModalBottomSheet(
-                            context: context,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.vertical(
-                              top: Radius.circular(20),
-                            )),
-                            builder: (context) => ProfileSheet(
-                                  currUserRef: widget.currUserData.userRef,
-                                  otherUserScore: creatorScore!,
-                                  otherUserFundName: localCreatorName,
-                                  otherUserFullDomain: localCreatorGroupName,
-                                  otherUserDomainColor: creatorGroupColor,
-                                  otherUserRef: widget.comment.creatorRef!,
-                                ));
-                      }
-                    },
-                    child: RichText(
-                      text: TextSpan(children: [
-                        TextSpan(
-                            text: localCreatorName + " • ",
-                            style: Theme.of(context)
-                                .textTheme
-                                .subtitle2
-                                ?.copyWith(color: colorScheme.primary, fontSize: commentReplyDetailSize)),
-                        TextSpan(
-                            text: localCreatorGroupName,
-                            style: Theme.of(context).textTheme.subtitle2?.copyWith(
-                                color: creatorGroupColor ?? colorScheme.primary,
-                                fontSize: commentReplyDetailSize))
-                      ]),
-                    )),
-              ),
-              Spacer(),
-              widget.comment.creatorRef == widget.postCreatorRef
-                  ? Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(17),
-                        color: Theme.of(context).colorScheme.surface,
-                      ),
-                      padding: EdgeInsets.fromLTRB(10, 5, 0, 1),
-                      margin: EdgeInsets.all(1),
-                      child: Text('Creator',
-                          style: Theme.of(context).textTheme.subtitle2?.copyWith(
-                              color: widget.groupColor ?? colorScheme.onSurface,
-                              fontSize: 12)))
-                  : Container(),
-            ],
-          ),
-          SizedBox(height: 4),
-          SizedBox(
-            width: (MediaQuery.of(context).size.width) * .85,
-            child: Text(widget.comment.text, style: Theme.of(context).textTheme.bodyText1),
-          ),
-          SizedBox(height: 7),
-          Row(
-            children: [
-              Text(convertTime(widget.comment.createdAt.toDate()),
-                  style: Theme.of(context)
-                      .textTheme
-                      .subtitle2
-                      ?.copyWith(color: colorScheme.primary, fontSize: commentReplyDetailSize)),
-              SizedBox(width: 8),
-              widget.comment.creatorRef != null && widget.comment.creatorRef != widget.currUserData.userRef
-                  ? Container(
-                      height: 30,
-                      child: IconButton(
-                        icon: Icon(Icons.more_horiz, size: 20, color: colorScheme.primary),
-                        alignment: Alignment.centerLeft,
-                        padding: EdgeInsets.zero,
+    return Column(
+      children: [
+        Container(
+          color: replySubject != false ? colorScheme.background : null,
+          padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Column(mainAxisSize: MainAxisSize.min, crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+              Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.fromLTRB(5.0,0.0,10.0,0.0),
+                    alignment: Alignment.centerLeft,
+                    height: 33,
+                    child: TextButton(
+                        style: TextButton.styleFrom(
+                            splashFactory: NoSplash.splashFactory,
+                            padding: EdgeInsets.zero,
+                            alignment: Alignment.centerLeft),
                         onPressed: () {
-                          showModalBottomSheet(
-                              context: context,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.vertical(
-                                top: Radius.circular(20),
-                              )),
-                              builder: (context) => ReportSheet(
-                                    reportType: ReportType.comment,
-                                    entityRef: widget.comment.commentRef,
-                                    entityCreatorRef: widget.comment.creatorRef!,
-                                  ));
+                          if (creatorName != null) {
+                            showModalBottomSheet(
+                                context: context,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.vertical(
+                                  top: Radius.circular(20),
+                                )),
+                                builder: (context) => ProfileSheet(
+                                      currUserRef: widget.currUserData.userRef,
+                                      otherUserScore: creatorScore!,
+                                      otherUserFundName: localCreatorName,
+                                      otherUserFullDomain: localCreatorGroupName,
+                                      otherUserDomainColor: creatorGroupColor,
+                                      otherUserRef: widget.comment.creatorRef!,
+                                    ));
+                          }
                         },
-                      ),
-                    )
-                  : Container(height: 30),
-              Spacer(),
-              Container(
-                height: 30,
-                child: TextButton(
-                    style: TextButton.styleFrom(
-                        splashFactory: NoSplash.splashFactory, padding: EdgeInsets.zero, alignment: Alignment.center),
-                    child: Text("Reply",
-                        style: Theme.of(context).textTheme.bodyText2?.copyWith(
-                            color: colorScheme.onSurface,
-                            fontWeight: FontWeight.w600,
-                            fontSize: commentReplyDetailSize + 1.5)),
-                    onPressed: () {
-                      widget.focusKeyboard();
-                      widget.switchFormBool(widget.comment);
-                    }),
+                        child: RichText(
+                          text: TextSpan(children: [
+                            TextSpan(
+                                text: localCreatorName + " • ",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .subtitle2
+                                    ?.copyWith(color: colorScheme.primary, fontSize: commentReplyDetailSize)),
+                            TextSpan(
+                                text: localCreatorGroupName,
+                                style: Theme.of(context).textTheme.subtitle2?.copyWith(
+                                    color: creatorGroupColor ?? colorScheme.primary,
+                                    fontSize: commentReplyDetailSize))
+                          ]),
+                        )),
+                  ),
+                  Spacer(),
+                  widget.comment.creatorRef == widget.postCreatorRef
+                      ? Container(
+                          color: Colors.transparent,
+                          padding: EdgeInsets.fromLTRB(10, 5, 0, 1),
+                          margin: EdgeInsets.all(1),
+                          child: Text('Creator',
+                              style: Theme.of(context).textTheme.subtitle2?.copyWith(
+                                  color: widget.groupColor ?? colorScheme.onSurface,
+                                  fontSize: 12)))
+                      : Container(),
+                ],
               ),
-              widget.comment.creatorRef != null
-                  ? LikeDislikeComment(
-                      comment: widget.comment,
-                      currUserRef: widget.currUserData.userRef,
-                      currUserColor: widget.currUserData.domainColor,
-                    )
-                  : Container()
-            ],
-          ),
-          SizedBox(height: 4),
-          Divider(thickness: 3, color: colorScheme.background, height: 0),
-          RepliesFeed(
-              commentRef: widget.comment.commentRef,
-              groupColor: widget.groupColor,
-              postCreatorRef: widget.postCreatorRef),
-        ]));
+              SizedBox(height: 4),
+              SizedBox(
+                width: (MediaQuery.of(context).size.width) * .85,
+                child: Text(widget.comment.text, style: Theme.of(context).textTheme.bodyText1),
+              ),
+              SizedBox(height: 7),
+              Row(
+                children: [
+                  Text(convertTime(widget.comment.createdAt.toDate()),
+                      style: Theme.of(context)
+                          .textTheme
+                          .subtitle2
+                          ?.copyWith(color: colorScheme.primary, fontSize: commentReplyDetailSize)),
+                  SizedBox(width: 8),
+                  widget.comment.creatorRef != null && widget.comment.creatorRef != widget.currUserData.userRef
+                      ? Container(
+                          height: 30,
+                          child: IconButton(
+                            icon: Icon(Icons.more_horiz, size: 20, color: colorScheme.primary),
+                            alignment: Alignment.centerLeft,
+                            padding: EdgeInsets.zero,
+                            onPressed: () {
+                              showModalBottomSheet(
+                                  context: context,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.vertical(
+                                    top: Radius.circular(20),
+                                  )),
+                                  builder: (context) => ReportSheet(
+                                        reportType: ReportType.comment,
+                                        entityRef: widget.comment.commentRef,
+                                        entityCreatorRef: widget.comment.creatorRef!,
+                                      ));
+                            },
+                          ),
+                        )
+                      : Container(height: 30),
+                  Spacer(),
+                  Container(
+                    height: 30,
+                    child: TextButton(
+                        style: TextButton.styleFrom(
+                            splashFactory: NoSplash.splashFactory, padding: EdgeInsets.zero, alignment: Alignment.center),
+                        child: Text("Reply",
+                            style: Theme.of(context).textTheme.bodyText2?.copyWith(
+                                color: colorScheme.onSurface,
+                                fontWeight: FontWeight.w600,
+                                fontSize: commentReplyDetailSize + 1.5)),
+                        onPressed: () {
+                          replySubject=true;
+                          widget.focusKeyboard();
+                          widget.switchFormBool(widget.comment);
+                        }),
+                  ),
+                  widget.comment.creatorRef != null
+                      ? LikeDislikeComment(
+                          comment: widget.comment,
+                          currUserRef: widget.currUserData.userRef,
+                          currUserColor: widget.currUserData.domainColor,
+                        )
+                      : Container()
+                ],
+              ),
+              SizedBox(height: 4),
+              Divider(thickness: 3, color: colorScheme.background, height: 0),
+            ]),),
+        RepliesFeed(
+            commentRef: widget.comment.commentRef,
+            groupColor: widget.groupColor,
+            postCreatorRef: widget.postCreatorRef),
+      ],
+    );
   }
 }
+
+
